ef8d7f4f3e57d9f889866e6a8ddabae583c46fd9
AmmrYsir <ammaryasir03062000@gmail.com>
2025-05-29 15:40:43 +0800

    [feat] create testing main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new().route("/", get(hello_handler));
+	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+	let listener = TcpListener::bind(addr).await.unwrap();
+	axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_handler() -> &'static str {
+	"Hello, World!"
 }
